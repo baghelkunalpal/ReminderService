3747f4197c57a7563161664cfad9eded920b8ad8
kunal pal <kunalpal2292004@gmail.com>
2023-07-24 17:53:42 +0530

    Added some notification cron jobs

--- src/utils/job.js
+++ src/utils/job.js
@@ -2,13 +2,6 @@ const cron = require('node-cron');
 const emailService = require('../services/email-service');
 const sender = require('../config/emailConfig');
 
-/**
- * 10:00 am 
- * Every 5 minutes
- * We will check are their any pending emails which was expected to be sent 
- * by now  and is pending
- */
-
 const setupJobs = () => {
     cron.schedule('*/2 * * * *', async () => {
         const response = await emailService.fetchPendingEmails();
