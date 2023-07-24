a4acdc47e1740d6e2addb1fa970e8700a1e79726
kunal pal <kunalpal2292004@gmail.com>
2023-07-24 17:37:24 +0530

    Added the ticket by email

--- src/index.js
+++ src/index.js
@@ -2,22 +2,21 @@ const express = require('express');
 const bodyPparser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 
-const {sendBasicEmail} = require('./services/email-service');
+// const {sendBasicEmail} = require('./services/email-service');
+
+const TicketController = require('./controllers/ticket-controller')
 
 const setupAndStartServer = ()=>{
     const app = express();
     app.use(bodyPparser.json());
     app.use(bodyPparser.urlencoded({extended:true}));
 
+    app.post('/api/v1/tickets',TicketController.create);
+
     app.listen(PORT, ()=>{
         console.log(`The server is started on port ${PORT}`);
-
-        sendBasicEmail(
-            //  'support@noti.com',
-            //  'recipient@example.com', // Set the recipient email address here
-            //  'Test Email',
-            //  'This is a test email sent using Nodemailer.',
-        )
+        // Jobs();
+        
     });
 }
 setupAndStartServer();
