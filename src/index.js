e3d3ffbff1818cd1434259d65add586bb9a72c03
kunal pal <kunalpal2292004@gmail.com>
2023-07-25 17:40:19 +0530

    Added the Rabit queue and config

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ const express = require('express');
 const bodyPparser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 
-// const {sendBasicEmail} = require('./services/email-service');
-
+const {  createChannel } = require('./utils/messageQueue');
+// const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 const TicketController = require('./controllers/ticket-controller')
-
-const setupAndStartServer = ()=>{
+const m = require('./models/notificationticket');
+// const jobs = require('./utils/job');
+const setupAndStartServer = async()=>{
     const app = express();
     app.use(bodyPparser.json());
     app.use(bodyPparser.urlencoded({extended:true}));
+    // const channel = await createChannel();
 
     app.post('/api/v1/tickets',TicketController.create);
 
     app.listen(PORT, ()=>{
         console.log(`The server is started on port ${PORT}`);
+       
         // Jobs();
         
     });
